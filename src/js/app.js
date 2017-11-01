1abe7ec7d308f0b06e7cc2bb12890b69675da2f2
Maksym <maks@coderiver.com.ua>
2017-11-01 14:13:14 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,17 @@ import $ from 'jquery';
 
 $(document).ready(function() {
   let select = $('.blabla');
-  let placeholder = $('<option />');
-  placeholder.html('Город*');
-  placeholder.attr('selected', 'selected');
-  placeholder.attr('hidden', 'hidden');
-  placeholder.attr('disabled', 'disabled');
-  select.append(placeholder);
+  let placeholder = $('.placeholder');
+  placeholder.attr('placeholder', 'Город*');
+  select.hide();
+  placeholder.show();
   let optionsList = select.find('option');
-  optionsList.hide();
   let optionsContainer = $('.options');
   for (let i = 0; i < optionsList.length - 1; i++) {
     let optionHtml = $(optionsList[i]).html();
     optionsContainer.append(`<li>${optionHtml}</li>`);
   }
-  select.on('click', function(event) {
+  placeholder.on('click', function(event) {
     event.stopPropagation();
     optionsContainer.toggle();
   });
@@ -26,6 +23,7 @@ $(document).ready(function() {
     optionsContainer.toggle();
     let target = event.target;
     optionsList.removeAttr('selected');
+    placeholder.attr('value', $(target).html());
     for (let i = 0; i < optionsList.length - 1; i++) {
       if ($(target).html() === $(optionsList[i]).html()) {
         $(optionsList[i]).attr('selected', 'selected');
